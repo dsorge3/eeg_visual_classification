a14b98610d44af538dc20d75823de9a268313103
dsorge3 <91691913+dsorge3@users.noreply.github.com>
2021-10-05 14:10:38 +0200

    Create EEGDatasetClass.py

---
+++ EEGDatasetClass.py
@@ -0,0 +1,34 @@
+# Dataset class
+class EEGDataset:
+    
+    # Constructor
+    def __init__(self, eeg_signals_path):
+        # Load EEG signals
+        loaded = torch.load(eeg_signals_path)
+        if opt.subject!=0:
+            self.data = [loaded['dataset'][i] for i in range(len(loaded['dataset']) ) if loaded['dataset'][i]['subject']==opt.subject]
+        else:
+            self.data=loaded['dataset']        
+        self.labels = loaded["labels"]
+        self.images = loaded["images"]
+        
+        # Compute size
+        self.size = len(self.data)
+
+    # Get size
+    def __len__(self):
+        return self.size
+
+    # Get item
+    def __getitem__(self, i):
+        # Process EEG
+        eeg = self.data[i]["eeg"].float().t()
+        eeg = eeg[opt.time_low:opt.time_high,:]
+
+        if opt.model_type == "model10":
+            eeg = eeg.t()
+            eeg = eeg.view(1,128,opt.time_high-opt.time_low)
+        # Get label
+        label = self.data[i]["label"]
+        # Return
+        return eeg, label
