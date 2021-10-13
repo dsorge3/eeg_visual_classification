e951eb96028ba17aca5a09240eece6b75aaf948e
dsorge3 <91691913+dsorge3@users.noreply.github.com>
2021-10-13 13:29:20 +0200

    Create splitterClass.py

---
+++ splitterClass.py
@@ -0,0 +1,24 @@
+# Splitter class
+class Splitter:
+
+    def __init__(self, dataset, split_path, split_num=0, split_name="train"):
+        # Set EEG dataset
+        self.dataset = dataset
+        # Load split
+        loaded = torch.load(split_path)
+        self.split_idx = loaded["splits"][split_num][split_name]
+        # Filter data
+        self.split_idx = [i for i in self.split_idx if 450 <= self.dataset.data[i]["eeg"].size(1) <= 600]
+        # Compute size
+        self.size = len(self.split_idx)
+
+    # Get size
+    def __len__(self):
+        return self.size
+
+    # Get item
+    def __getitem__(self, i):
+        # Get sample from dataset
+        eeg, label = self.dataset[self.split_idx[i]]
+        # Return
+        return eeg, label
