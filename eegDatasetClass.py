66f75a4ab1ae73b998329ba6959484c731ec55ae
dsorge3 <91691913+dsorge3@users.noreply.github.com>
2021-10-21 14:59:48 +0200

    Update eegDatasetClass.py

--- eegDatasetClass.py
+++ eegDatasetClass.py
@@ -1,39 +1,3 @@
-# Define options
-import argparse
-
-parser = argparse.ArgumentParser(description="Template")
-# Dataset options
-
-# Data - Data needs to be pre-filtered and filtered data is available
-
-# Model type/options
-parser.add_argument('-mt', '--model_type', default='lstm',
-                    help='specify which generator should be used: lstm|EEGChannelNet')
-# It is possible to test out multiple deep classifiers:
-# - lstm is the model described in the paper "Deep Learning Human Mind for Automated Visual Classification”, in CVPR 2017
-# - model10 is the model described in the paper "Decoding brain representations by multimodal learning of neural activity and visual features", TPAMI 2020
-parser.add_argument('-mp', '--model_params', default='', nargs='*', help='list of key=value pairs of model options')
-parser.add_argument('--pretrained_net', default='', help="path to pre-trained net (to continue training)")
-
-# Training options
-parser.add_argument("-b", "--batch_size", default=16, type=int, help="batch size")
-parser.add_argument('-o', '--optim', default="Adam", help="optimizer")
-parser.add_argument('-lr', '--learning-rate', default=0.001, type=float, help="learning rate")
-parser.add_argument('-lrdb', '--learning-rate-decay-by', default=0.5, type=float, help="learning rate decay factor")
-parser.add_argument('-lrde', '--learning-rate-decay-every', default=10, type=int, help="learning rate decay period")
-parser.add_argument('-dw', '--data-workers', default=4, type=int, help="data loading workers")
-parser.add_argument('-e', '--epochs', default=200, type=int, help="training epochs")
-
-# Save options
-parser.add_argument('-sc', '--saveCheck', default=100, type=int, help="learning rate")
-
-# Backend options
-parser.add_argument('--no-cuda', default=False, help="disable CUDA", action="store_true")
-
-# Parse arguments
-opt = parser.parse_args()
-print(opt)
-
 # Imports
 import torch; torch.utils.backcompat.broadcast_warning.enabled = True
 import torch.optim
@@ -68,7 +32,7 @@ class EEGDataset:
         eeg = self.data[i]["eeg"].float().t()
         eeg = eeg[args.time_low:args.time_high, :]
 
-        if opt.model_type == "model10":
+        if args.model_type == "model10":
             eeg = eeg.t()
             eeg = eeg.view(1, 128, args.time_high - args.time_low)
         # Get label
